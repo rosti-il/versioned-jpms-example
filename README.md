3332767ea445424200e5212f813426c32a032adb
Rostislav Krasny <45571812+rosti-il@users.noreply.github.com>
2020-01-18 21:04:18 +0200

    Improve

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ### versioned-jpms-example
 
 A simple example of using JPMS layers to support versioning of transitive dependencies.
+
+You need JDK 11 or newer to build and run this example.
+
+Maven version 3.6.3 was used.
--- versioned-jpms/src/main/java/com/github/rosti_il/jpms/VersionedModuleProvider.java
+++ versioned-jpms/src/main/java/com/github/rosti_il/jpms/VersionedModuleProvider.java
@@ -13,20 +13,22 @@ public class VersionedModuleProvider {
         var parent = callerModule.getLayer();
         var versionedModules = callerModule.getAnnotation(VersionedModules.class);
 
-        for (String moduleDef : versionedModules.modules()) {
-            var delimiterIdx = moduleDef.indexOf(':');
-            var module = moduleDef.substring(0, delimiterIdx);
-            var from = Paths.get(module + '-' + moduleDef.substring(delimiterIdx + 1) + ".jar");
-            var cf = parent.configuration().resolve(ModuleFinder.of(from), ModuleFinder.of(), Set.of(module));
-            var layer = parent.defineModulesWithOneLoader(cf, ClassLoader.getSystemClassLoader());
-            var services = ServiceLoader.load(layer, VersionedService.class);
+        if (versionedModules != null) {
+            for (String moduleDef : versionedModules.modules()) {
+                var delimiterIdx = moduleDef.indexOf(':');
+                var module = moduleDef.substring(0, delimiterIdx);
+                var from = Paths.get(module + '-' + moduleDef.substring(delimiterIdx + 1) + ".jar");
+                var cf = parent.configuration().resolve(ModuleFinder.of(from), ModuleFinder.of(), Set.of(module));
+                var layer = parent.defineModulesWithOneLoader(cf, ClassLoader.getSystemClassLoader());
+                var services = ServiceLoader.load(layer, VersionedService.class);
 
-            VersionedService service = services.findFirst().get();
-            if (!clazz.isInstance(service)) {
-                continue;
+                VersionedService service = services.findFirst().get();
+                if (!clazz.isInstance(service)) {
+                    continue;
+                }
+
+                return (T) service;
             }
-
-            return (T) service;
         }
 
         throw new RuntimeException("module not found");
